3aad0a13ca331b45f91170424c2fc2134036d416
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-08-16 09:13:18 +0930

    Added next_button.

--- scraper.rb
+++ scraper.rb
@@ -20,7 +20,8 @@ p first_page.title.strip
 first_page_form = first_page.forms.first
 # select the "List of Development Applications" radio button
 first_page_form.radiobuttons[0].click
-search_page = agent.submit(first_page_form, nil, { '__VIEWSTATEENCRYPTED' => '' })
+next_button = search_form.button_with(:value => "Next")
+search_page = agent.submit(first_page_form, next_button, { '__VIEWSTATEENCRYPTED' => '' })
 # search_page = first_page_form.click_button
 
 # select the "Date Lodged" tab
