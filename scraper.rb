8b076494766eb330ef57183f53571a1263a81a35
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-08-16 09:09:42 +0930

    Changed initial navigation.

--- scraper.rb
+++ scraper.rb
@@ -20,7 +20,8 @@ p first_page.title.strip
 first_page_form = first_page.forms.first
 # select the "List of Development Applications" radio button
 first_page_form.radiobuttons[0].click
-search_page = first_page_form.click_button
+agent.submit(first_page_form, nil, { '__VIEWSTATEENCRYPTED'] => '' })
+# search_page = first_page_form.click_button
 
 # select the "Date Lodged" tab
 search_form = search_page.forms.first
