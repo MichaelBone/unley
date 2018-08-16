0d43dc9c3bce12824a7fb36dd2100219a32b4f5e
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-08-16 09:46:00 +0930

    Test counter.

--- scraper.rb
+++ scraper.rb
@@ -89,8 +89,8 @@ while summary_page
     next_page_path = next_page_img['onclick'].split(',').find { |e| e =~ /.*PageNumber=\d+.*/ }.gsub('"', '').strip
     p "Found another page: " + next_page_path
     summary_page = agent.get "#{base_url}#{next_page_path}"
-    count = count + 1
-    if count > 2
+    count += 1
+    if count > 1
       p "Stopping paging after " + count + "pages."
       break
     end
