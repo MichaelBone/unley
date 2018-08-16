8f97043733f34b238b5924c481756efcd1bb9f9c
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-08-16 10:02:46 +0930

    Removed event argument and target.

--- scraper.rb
+++ scraper.rb
@@ -30,8 +30,8 @@ search_page = first_page_form.click_button
 
 # select the "Date Lodged" tab
 search_form = search_page.forms.first
-search_form['__EVENTTARGET'] = 'ctl00$MainBodyContent$mGeneralEnquirySearchControl$mTabControl$tabControlMenu'
-search_form['__EVENTARGUMENT'] = '3'
+# search_form['__EVENTTARGET'] = 'ctl00$MainBodyContent$mGeneralEnquirySearchControl$mTabControl$tabControlMenu'
+# search_form['__EVENTARGUMENT'] = '3'
 #search_form['__LASTFOCUS'] = ''
 #search_form['ctl00$MainBodyContent$mGeneralEnquirySearchControl$mEnquiryListsDropDownList'] = '10'
 #search_form['ctl00$MainBodyContent$mGeneralEnquirySearchControl$mTabControl$ctl04$mStreetNameTextBox'] = ''
@@ -70,7 +70,7 @@ while summary_page
     p "Found another page: " + next_page_path
     summary_page = agent.get "#{base_url}#{next_page_path}"
     count += 1
-    if count > 100
+    if count > 10
       p "Stopping paging after " + count.to_s + " pages."
       break
     end
